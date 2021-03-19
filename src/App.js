d19cacdd871a837fb13dbb1200392d180b24ef63
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-19 11:59:43 +0530

    *** used componentDidMount() method property in function component using 'useEffect' hook and here setTimeout() method works fine but needed dependency to be passed.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 function App() {
 
-  const [value,setValue] = useState(null);
+  const [value,setValue] = useState(10);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setValue(value-1);
+    },1000)
+  },[value])
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-12">
           <h1>Rishav</h1>
-          {JSON.stringify(value,null,2)}
+          <pre className="border rounded p-1 bg-light">
+            {JSON.stringify(value,null,2)}
+          </pre>
         </div>
       </div>
     </div>
