25b2df7938e041278898f9ce14b68623573136db
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-19 22:23:16 +0530

    In react for converted into htmlFor and class converted into className.

--- src/App.js
+++ src/App.js
@@ -19,6 +19,24 @@ function App() {
             <button className="btn btn-primary m-3" onClick={handleHeader}> dark</button>
             <button className="btn btn-primary m-3" onClick={handleHeader}>light</button>
           </div>
+          <div className="col-6 offset-3">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1"/>
+              </div>
+              <div className="mb-3 form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
