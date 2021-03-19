337e983426f2c6d4c8b007bdef2f1ddc201850bc
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-19 12:14:40 +0530

    Another way of defining state in function component.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ function App() {
 
   const [value,setValue] = useState(10);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setValue(value-1);
-    },1000)
-  },[value])
+  const uState = useState(15);
+  const state = uState[0];
+  const setState = uState[1];
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setValue(value-1);
+  //   },1000)
+  // },[value])
 
   return (
     <div className="container">
@@ -16,7 +20,7 @@ function App() {
         <div className="col-12">
           <h1>Rishav</h1>
           <pre className="border rounded p-1 bg-light">
-            {JSON.stringify(value,null,2)}
+            {JSON.stringify({value,state},null,2)}
           </pre>
         </div>
       </div>
