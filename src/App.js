206dd477e136abaf17a196aa6d7fa2e2d3db5e28
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-19 18:53:27 +0530

    Get back to the Boiler plate for learning new thing.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import React, {useState, useEffect} from 'react'
-import {getTodos} from "./services/ToDo"
+import React from 'react'
 
 function App() {
-
-  const [value,setValue] = useState(10);
-  const [todos,setTodos] = useState();
-
-  useEffect(() => {
-    getTodos().then((json) => setTodos(json.data))
-  },)      
-
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <h1>Rishav</h1>
-          <pre className="border rounded p-1 bg-light">
-            {JSON.stringify({value,todos},null,2)}
-          </pre>
-        </div>
-      </div>
+    <div>
+      Rishav
     </div>
   )
 }
 
 export default App
+
+
+
+
+
+
+// import React, {useState, useEffect} from 'react'
+// import {getTodos} from "./services/ToDo"
+
+// function App() {
+
+//   const [value,setValue] = useState(10);
+//   const [todos,setTodos] = useState();
+
+//   useEffect(() => {
+//     getTodos().then((json) => setTodos(json.data))
+//   },)      
+
+//   return (
+//     <div className="container">
+//       <div className="row">
+//         <div className="col-12">
+//           <h1>Rishav</h1>
+//           <pre className="border rounded p-1 bg-light">
+//             {JSON.stringify({value,todos},null,2)}
+//           </pre>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App
