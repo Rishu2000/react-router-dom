72fb1a7d45af9e1c3098026ee1d4c02fe90249b9
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-19 11:46:25 +0530

    used state property in function component using 'useState' hook.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function App() {
+
+  const [value,setValue] = useState(null);
+
   return (
-    <div>
-      Rishav
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h1>Rishav</h1>
+          {JSON.stringify(value,null,2)}
+        </div>
+      </div>
     </div>
   )
 }
