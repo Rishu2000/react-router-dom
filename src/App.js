723a817cfdb2fa741953a904ec46ed10d29f45b1
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-19 23:00:34 +0530

    Another example of controled component and conclude that controled component can be dependent on state.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Header from "./components/Header"
 function App() {
 
   const [Dark,setDark] = useState(true);
-
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+ 
   const handleHeader = (e) => {
     e.preventDefault();
       setDark(e.target.innerHTML.trim() === "dark");  //.trim() function used to remove white-space from both side.
@@ -19,16 +21,31 @@ function App() {
             <button className="btn btn-primary m-3" onClick={handleHeader}> dark</button>
             <button className="btn btn-primary m-3" onClick={handleHeader}>light</button>
           </div>
-          <div className="col-6 offset-3">
+          <div className="col-6">
             <form>
               <div className="mb-3">
                 <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                <input 
+                  type="email" 
+                  className="form-control" 
+                  id="exampleInputEmail1" 
+                  aria-describedby="emailHelp"
+                  value={email} 
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }} />
+                <small id="emailHelp" className="form-text">We'll never share your email with anyone else.</small>
               </div>
               <div className="mb-3">
                 <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1"/>
+                <input 
+                  type="password" 
+                  className="form-control" 
+                  id="exampleInputPassword1"
+                  value={password} 
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }} />
               </div>
               <div className="mb-3 form-check">
                 <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
@@ -36,6 +53,11 @@ function App() {
               </div>
               <button type="submit" className="btn btn-primary">Submit</button>
             </form>
+          </div>
+          <div className="col-6">
+            <pre className="border rounded bg-light p-3 m-4">
+              {JSON.stringify({Dark,email,password},null,2)}
+            </pre>
           </div>
         </div>
       </div>
